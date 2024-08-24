078a44cc1732a44a5f912aecd5962afbc4696f76
Kavipaka <kavindhagihan01@gmail.com>
2024-08-24 09:50:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=rV9A3CzA#oQJjamGytDHv72jsVH1PMpQ50YJuX2f5vVDluL1V170' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=jMkUxIiZ#GBLWSNpPWqCtTryvgLClYEE3kv7Moo8zI1OoU2glwcQ' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
